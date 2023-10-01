57f78d01625c241b40f189d1215a242517e8ee18
natsuki <su811sh1@gmail.com>
2023-10-01 23:25:01 +0900

    add: チャプター34のファイル作成

---
+++ form/input_34.php
@@ -0,0 +1,29 @@
+<?php
+  if (!empty($_POST)){
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form method="POST" action="input.php">
+    氏名
+    <input type="text" name="your_name">
+    <br>
+    <input type="checkbox" name="sports[]" value="野球">野球
+    <input type="checkbox" name="sports[]" value="サッカー">サッカー
+    <input type="checkbox" name="sports[]" value="バスケ">バスケ
+    <br>
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
