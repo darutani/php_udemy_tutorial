271eb0f6b8eb28e197cfbe0dbd00df4a7b27b6e3
natsuki <su811sh1@gmail.com>
2023-10-08 15:29:55 +0900

    add: チャプター67

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hayashinatsuki/composertest',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2ce31aadc295642c14f50390f8f7fe812e475558',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hayashinatsuki/composertest' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2ce31aadc295642c14f50390f8f7fe812e475558',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
